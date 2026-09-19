cf0bc0cfc5d5d85a6fdd789d753996d14998485f
CasperBohlin <109875785+Exedze@users.noreply.github.com>
2026-09-19 17:02:38 +0200

    water added

---
+++ Assets/Scripts/carmeraFollow.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class carmeraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
