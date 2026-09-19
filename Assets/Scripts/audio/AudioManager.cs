390331c7776629d4c1afdb56533b7b3660ef907e
99-LiWe <81622251+99-LiWe@users.noreply.github.com>
2026-09-19 17:36:41 +0200

    Update AudioManager.cs

--- Assets/Scripts/audio/AudioManager.cs
+++ Assets/Scripts/audio/AudioManager.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
 using FMODUnity;
+using JetBrains.Annotations;
 
-public class AudioManager : MonoBehaviour
+public class AudioManager : ScriptableObject
 {
- 
+    private FMOD.Studio.EventInstance soundEvent;
+   [SerializeField] private string paramName;
+   [SerializeField] private string path;
+   [SerializeField] private float paramValue;
+   [SerializeField] private bool loop;
+   
+   
 }
