3f8bc65a54d526f0997270a65626e2f0d4cb7315
CasperBohlin <109875785+Exedze@users.noreply.github.com>
2026-09-19 16:08:42 +0200

    drowning added

---
+++ Assets/Scripts/waters/Drowning.cs
@@ -0,0 +1,74 @@
+using System;
+using UnityEngine;
+
+public class Drowning : MonoBehaviour
+{
+    private int sources = 0;
+    public bool headInWater;
+    
+    [SerializeField]
+    private float drowningCurrent=0;
+    [SerializeField]
+    private float drowningMax = 20;
+    [SerializeField] private float breatheInSpeed=2;
+
+    public float DrowningProgress
+    {
+        get
+        {
+            return (drowningCurrent / drowningMax);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sources > 0)
+        {
+            EnterWater();
+        }
+        else
+        {
+            ExitWater();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Water")
+        {
+            sources += 1;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Water")
+        {
+            sources -= 1;
+        }
+    }
+
+    private void EnterWater()
+    {
+        if (!headInWater)
+        {
+            
+        }
+
+        drowningCurrent += Time.deltaTime;
+        headInWater = true;
+    }
+
+    private void ExitWater()
+    {
+        if (headInWater)
+        {
+            
+        }
+
+        drowningCurrent -= Time.deltaTime*breatheInSpeed;
+        if (drowningCurrent < 0) drowningCurrent = 0;
+        headInWater = false;
+    }
+}
