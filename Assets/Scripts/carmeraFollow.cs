9fad05b37108d76200603848a0c56104b7bbbca1
CasperBohlin <109875785+Exedze@users.noreply.github.com>
2026-09-19 17:13:36 +0200

    camera fixes

--- Assets/Scripts/carmeraFollow.cs
+++ Assets/Scripts/carmeraFollow.cs
@@ -12,6 +12,6 @@ public class carmeraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(target.transform.position.x,target.transform.position.y,transform.position.z);
     }
 }
